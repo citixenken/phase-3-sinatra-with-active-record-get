ec2e8cea26f64ea3647058fdc2fe3dd88fc40203
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-30 07:05:32 +0300

    COMPLETED: sinatra with ar-GET req

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,39 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  # set content-type for all responses
+  set :default_content_type, 'application/json'
+
+  # get '/' do
+  #   { message: "Hello world" }.to_json
+  # end
+
+  get '/games' do
+    # get all games from DB
+    # games = Game.all
+    # games = Game.all.sort_by(&:price).reverse
+    # games = Game.all.order(:price).limit(10).reverse_order
+    # games = Game.all.order(price: :desc).limit(10)
+
+    games = Game.all.order(:title)
+
+    # return a JSON response with an array of all game data
+    games.to_json
+  end
+
+  #use :id syntax to create a dynamic route
+  get '/games/:id' do
+    # look up the game in the database using its ID
+    game = Game.find(params[:id])
+
+    # send a JSON-formatted response of the game data
+    # game.to_json
+
+    #include associated reviews in JSON response
+    # game.to_json(include: :reviews)
+
+    #include users associated with each review in JSON response
+    game.to_json(only: [:id, :title, :genre, :price], include: { reviews: { include: :user} })
+
   end
 
 end
